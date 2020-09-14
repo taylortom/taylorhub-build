1a8ee51abe4f82694eb0a38a449a049d8fd1ec65
Tom Taylor <hello@tomtaylor.name>
2020-09-14 21:52:27 +0100

    Update files

--- run.js
+++ run.js
@@ -1,22 +1,31 @@
-const { exec } = require('child_process');
+const childProcess = require('child_process');
+const fs = require('fs-extra');
+const path = require('path');
 
-async function buildServer() {
-  console.log('buildServer');
+async function exec(cmd, cwd) {
+  return new Promise((resolve, reject) => childProcess.exec(cmd, { cwd }).on('error', reject).on('exit', resolve));
 }
 
-async function buildUI() {
-  console.log('buildUI');
+async function copyDir(src, dirname = path.basename(src)) {
+  const excludes = [
+    '.git',
+    'node_modules'
+  ];
+  return fs.copy(src, dirname, { filter: f => !excludes.includes(path.basename(f)) });
 }
-
-async function commit() {
-  console.log('commit');
-}
-
 async function run() {
+  const serverDir = path.resolve('../server');
+  const uiDir = path.resolve('../ui');
   console.log('Running build');
-  await Promise.all([buildServer, buildUI]);
-  await commit();
-  console.log('done!');
+  try {
+    await exec(`npm run build`, uiDir);
+    await copyDir(serverDir);
+    await copyDir(`${uiDir}/build`, 'ui');
+    await exec(`git commit -m "Update build from run script"`);
+    console.log('done!');
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 run();
