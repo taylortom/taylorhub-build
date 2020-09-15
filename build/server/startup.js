c1150d3d7ed7cc509531c449eec78ad66d5c9cf9
Tom Taylor <hello@tomtaylor.name>
2020-09-15 15:32:57 +0100

    Update build

--- build/server/startup.js
+++ build/server/startup.js
@@ -1,7 +1,8 @@
 const { exec } = require('child_process');
+const path = require('path');
 const Server = require('./src/server');
 
-const config = require('../config');
+const config = require(path.resolve(__dirname, '../config'));
 
 async function startup() {
   const server = new Server(config);
