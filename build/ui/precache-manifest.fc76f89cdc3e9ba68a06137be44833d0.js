9ef4bb13abce519dcce83db8be67c213ba407acd
Tom Taylor <hello@tomtaylor.name>
2020-09-27 17:23:18 +0100

    Update build from run script

---
+++ build/ui/precache-manifest.fc76f89cdc3e9ba68a06137be44833d0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2113eb0ebfeefcb8158b2995f00b203a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cdd86e93f05107ba302",
+    "url": "/static/css/main.80068fab.chunk.css"
+  },
+  {
+    "revision": "3a95c16d2dffc3c7ae6d",
+    "url": "/static/js/2.f355d9fa.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/static/js/2.f355d9fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cdd86e93f05107ba302",
+    "url": "/static/js/main.683dc790.chunk.js"
+  },
+  {
+    "revision": "dd1c8ebec79ca6d0747d",
+    "url": "/static/js/runtime-main.41591dab.js"
+  },
+  {
+    "revision": "7125fb4ababc582e2c19f6c9bef21e71",
+    "url": "/static/media/050-hamburguer.7125fb4a.svg"
+  },
+  {
+    "revision": "e5b80f321923f49b5585b36d6ea7bc8c",
+    "url": "/static/media/081-food.e5b80f32.svg"
+  },
+  {
+    "revision": "0a769d09a06d135642f2f97b81ce8dd6",
+    "url": "/static/media/082-turkey.0a769d09.svg"
+  },
+  {
+    "revision": "2bd676c656f824e5917a4959ccb8ea55",
+    "url": "/static/media/086-steak.2bd676c6.svg"
+  },
+  {
+    "revision": "32c317f6b1e2c35d0e546114df110b40",
+    "url": "/static/media/101-kebab.32c317f6.svg"
+  },
+  {
+    "revision": "70fcaefb4f1e62aff020aa5226d40f23",
+    "url": "/static/media/104-pizza.70fcaefb.svg"
+  },
+  {
+    "revision": "4f52699c8a5c3c5a892bdeb5c6f7de14",
+    "url": "/static/media/108-sausage.4f52699c.svg"
+  },
+  {
+    "revision": "c412873df4980829f372f1128be5990b",
+    "url": "/static/media/127-rice.c412873d.svg"
+  },
+  {
+    "revision": "3a42a18cac5666f917e4d07c597abdd2",
+    "url": "/static/media/135-fish.3a42a18c.svg"
+  },
+  {
+    "revision": "749f958ba11979c0a355c7eb8243c846",
+    "url": "/static/media/145-potatoes.749f958b.svg"
+  },
+  {
+    "revision": "c72327d6d07600ee9e8ea441378cdca4",
+    "url": "/static/media/147-carrot.c72327d6.svg"
+  },
+  {
+    "revision": "ba10dad3e72f913a8696dc91e206d956",
+    "url": "/static/media/203-spaguetti.ba10dad3.svg"
+  }
+]);
--- build/ui/service-worker.js
+++ build/ui/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3f859e944b8c3558824f7b7aea78f9f8.js"
+  "/precache-manifest.fc76f89cdc3e9ba68a06137be44833d0.js"
 );
 
 self.addEventListener('message', (event) => {
