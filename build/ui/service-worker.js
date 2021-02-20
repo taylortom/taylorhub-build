656dfd99da089ebcc2515523e5dc2c811c21918c
Tom Taylor <hello@tomtaylor.name>
2021-02-20 14:26:17 +0000

    Update build from run script

--- build/ui/service-worker.js
+++ build/ui/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2dc17f51b65adef4e6d820e9f9d39c88.js"
+  "/precache-manifest.f1497f584a57ba379737e51a2adef6b7.js"
 );
 
 self.addEventListener('message', (event) => {
